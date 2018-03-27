3b56f1f0c568bc677362b339ffa67b1b3402a5d4
Stanislav Pankevich <s.pankevich@gmail.com>
2018-03-27 19:55:30 +0200

    Objective-C: Simple test for calling a class method.

--- lab-jit-objc/llvm-jit-lab/src/TestObjC.cpp
+++ lab-jit-objc/llvm-jit-lab/src/TestObjC.cpp
@@ -203,3 +203,61 @@ TEST(LLVMJIT, Test001_BasicTest) {
   int result = runnerFunction();
   EXPECT_EQ(result, 1234);
 }
+
+TEST(LLVMJIT, Test002_ClassMethodCall) {
+    // These lines are needed for TargetMachine TM to be created correctly.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
+
+  llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+                                                      "/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation"
+                                                      ));
+
+  llvm::LLVMContext llvmContext;
+
+  char fixturePath[255];
+  snprintf(fixturePath, sizeof(fixturePath), "%s/%s", FixturesPath, "002_calling_class_method.bc");
+
+  auto objcModule = loadModuleAtPath(fixturePath, llvmContext);
+
+  ObjectLinkingLayer<> ObjLayer;
+
+  std::unique_ptr<TargetMachine> TM(
+                                    EngineBuilder().selectTarget(llvm::Triple(), "", "", SmallVector<std::string, 1>()));
+
+  assert(TM.get());
+
+  SimpleCompiler compiler(*TM);
+
+  auto objcCompiledModule = compiler(*objcModule);
+
+  std::vector<object::ObjectFile*> objcSet;
+  objcSet.push_back(objcCompiledModule.getBinary());
+
+  ObjcResolver objcResolver;
+  auto objcHandle = ObjLayer.addObjectSet(std::move(objcSet),
+                                          make_unique<ObjCEnabledMemoryManager>(),
+                                          &objcResolver);
+
+  ObjLayer.emitAndFinalize(objcHandle);
+
+  std::string functionName = "_run";
+  JITSymbol symbol = ObjLayer.findSymbol(functionName, false);
+
+  void *fpointer =
+  reinterpret_cast<void *>(static_cast<uintptr_t>(symbol.getAddress()));
+
+  if (fpointer == nullptr) {
+    errs() << "CustomTestRunner> Can't find pointer to function: "
+    << functionName << "\n";
+    exit(1);
+  }
+
+  auto runnerFunction = ((int (*)(void))(intptr_t)fpointer);
+
+  int result = runnerFunction();
+  EXPECT_EQ(result, 1234);
+}
