2da9119cbb6b11e7e19cb068ea8f495384758ad7
Stanislav Pankevich <s.pankevich@gmail.com>
2018-03-30 11:16:10 +0200

    Minimal Swift Xcode project to test things out.

---
+++ lab-jit-objc/fixtures/swift-xcode-project/swift-xcode-project-tests/MinimalSwiftXCTestCase.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+class MinimalSwiftXCTestCase: XCTestCase {
+  func testExample() {
+    XCTAssertTrue(true);
+  }
+}
+
---
+++ lab-jit-objc/fixtures/swift-xcode-project/swift-xcode-project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-xcode-project
+//
+//  Created by Stanislav Pankevich on 30.03.18.
+//  Copyright © 2018 Stanislav Pankevich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
