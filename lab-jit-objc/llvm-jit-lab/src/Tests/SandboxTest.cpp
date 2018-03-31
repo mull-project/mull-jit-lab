3a19afab34ba23f0f9e9cc70138b576f4699b2bf
Stanislav Pankevich <s.pankevich@gmail.com>
2018-03-31 19:00:14 +0200

    Objective-C: Minimal test running XCTest/ObjC.

--- lab-jit-objc/llvm-jit-lab/src/Tests/SandboxTest.cpp
+++ lab-jit-objc/llvm-jit-lab/src/Tests/SandboxTest.cpp
@@ -16,7 +16,9 @@
 using namespace llvm;
 using namespace llvm::orc;
 
-TEST(DISABLED_LLVMJIT, ObjCRegistration) {
+const char *const FixturesPath = "/opt/mull-jit-lab/lab-jit-objc/fixtures/bitcode";
+
+TEST(XCTest_ObjC, Test_001_Minimal) {
   // These lines are needed for TargetMachine TM to be created correctly.
   llvm::InitializeNativeTarget();
   llvm::InitializeNativeTargetAsmPrinter();
@@ -24,33 +26,39 @@ TEST(DISABLED_LLVMJIT, ObjCRegistration) {
 
   llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
 
-  //assert(!sys::DynamicLibrary::LoadLibraryPermanently("/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation"));
-  assert(!sys::DynamicLibrary::LoadLibraryPermanently("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/XCTest.framework/XCTest"));
-  assert(!sys::DynamicLibrary::LoadLibraryPermanently("/opt/MUT_XCTestDriver.dylib"));
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+    "/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation"
+  ));
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+    "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/XCTest.framework/XCTest"
+  ));
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+    "/Users/Stanislaw/rootstanislaw/projects/CustomXCTestRunner/CustomXCTestRunner.dylib"
+  ));
 
   llvm::LLVMContext llvmContext;
 
-  auto objcModule             = loadModuleAtPath("/opt/jitobjc.bc",   llvmContext);
-  auto mutatedSomeClassModule = loadModuleAtPath("/opt/SomeClass.bc", llvmContext);
+  char fixturePath[255];
+  snprintf(fixturePath, sizeof(fixturePath), "%s/%s", FixturesPath, "xctest_objc_001_minimal_xctestcase_run.bc");
+
+  auto objcModule = loadModuleAtPath(fixturePath, llvmContext);
 
   ObjectLinkingLayer<> ObjLayer;
 
   std::unique_ptr<TargetMachine> TM(
-    EngineBuilder().selectTarget(llvm::Triple(), "", "",
-    SmallVector<std::string, 1>()));
+    EngineBuilder().selectTarget(llvm::Triple(),
+                                 "",
+                                 "",
+                                 SmallVector<std::string, 1>())
+  );
 
   assert(TM.get());
 
   SimpleCompiler compiler(*TM);
 
   auto objcCompiledModule = compiler(*objcModule);
-  auto mutatedSomeClassCompiledModule = compiler(*mutatedSomeClassModule);
-
 
   std::vector<object::ObjectFile*> objcSet;
-  object::ObjectFile *someClassObject = mutatedSomeClassCompiledModule.getBinary();
-
-  objcSet.push_back(someClassObject);
   objcSet.push_back(objcCompiledModule.getBinary());
 
   ObjCResolver objcResolver;
@@ -60,28 +68,7 @@ TEST(DISABLED_LLVMJIT, ObjCRegistration) {
 
   ObjLayer.emitAndFinalize(objcHandle);
 
-  std::string functionName = "_objc_function";
-  JITSymbol symbol = ObjLayer.findSymbol(functionName, false);
-
-  void *fpointer =
-  reinterpret_cast<void *>(static_cast<uintptr_t>(symbol.getAddress()));
-
-  if (fpointer == nullptr) {
-    errs() << "CustomTestRunner> Can't find pointer to function: "
-    << functionName << "\n";
-    exit(1);
-  }
-
-  void *mainPointer = fpointer;
-  auto objcFunction = ((void (*)(void))(intptr_t)mainPointer);
-
-  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
-//  objcFunction();
+  void *runnerPtr = sys::DynamicLibrary::SearchForAddressOfSymbol("CustomXCTestRunnerRun");
+  auto runnerFPtr = ((int (*)(void))runnerPtr);
+  runnerFPtr();
 }
