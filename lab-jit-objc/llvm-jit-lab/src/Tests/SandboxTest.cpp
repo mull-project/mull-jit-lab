b13af20703f6f36e6a27969eb6e5346d4e2932b3
Stanislav Pankevich <s.pankevich@gmail.com>
2018-04-02 18:53:09 +0200

    WIP: Fuzzer: todo: to implement 2 objc property attrs.

--- lab-jit-objc/llvm-jit-lab/src/Tests/SandboxTest.cpp
+++ lab-jit-objc/llvm-jit-lab/src/Tests/SandboxTest.cpp
@@ -44,11 +44,28 @@ TEST(Sandbox, Test001) {
 
   llvm::LLVMContext llvmContext;
 
-  std::string path_RunnerTests = getFixturePath("bitcode/FuzzerTests/Runner/RunnerTests.bc");
-  std::string path_Report = getFixturePath("bitcode/Fuzzer/Reports/FZRReport.bc");
+  std::string path_RunnerTests              = getFixturePath("bitcode/FuzzerTests/Runner/RunnerTests.bc");
+  std::string path_DeleteNodeMutationTests  = getFixturePath("bitcode/FuzzerTests/Mutations/DeleteNodeMutationTests.bc");
+  std::string path_ReplaceNodeMutationTests = getFixturePath("bitcode/FuzzerTests/Mutations/ReplaceNodeMutationTests.bc");
 
-  auto module_runnerTests = loadModuleAtPath(path_RunnerTests, llvmContext);
-  auto module_report = loadModuleAtPath(path_Report, llvmContext);
+  std::string path_Report              = getFixturePath("bitcode/Fuzzer/Reports/FZRReport.bc");
+  std::string path_NodeReplacement     = getFixturePath("bitcode/Fuzzer/Replacements/FZRNodeReplacement.bc");
+  std::string path_Runner              = getFixturePath("bitcode/Fuzzer/Runners/FZRRunner.bc");
+  std::string path_MutationFactory     = getFixturePath("bitcode//Fuzzer/Factories/FZRMutationFactory.bc");
+  std::string path_DeleteNodeMutation  = getFixturePath("bitcode//Fuzzer/Mutations/FZRDeleteNodeMutation.bc");
+  std::string path_ReplaceNodeMutation = getFixturePath("bitcode//Fuzzer/Mutations/FZRReplaceNodeMutation.bc");
+
+
+  auto module_runnerTests              = loadModuleAtPath(path_RunnerTests, llvmContext);
+  auto module_ReplaceNodeMutationTests = loadModuleAtPath(path_ReplaceNodeMutationTests, llvmContext);
+  auto module_DeleteNodeMutationTests  = loadModuleAtPath(path_DeleteNodeMutationTests, llvmContext);
+
+  auto module_report              = loadModuleAtPath(path_Report, llvmContext);
+  auto module_NodeReplacement     = loadModuleAtPath(path_NodeReplacement, llvmContext);
+  auto module_Runner              = loadModuleAtPath(path_Runner, llvmContext);
+  auto module_MutationFactory     = loadModuleAtPath(path_MutationFactory, llvmContext);
+  auto module_DeleteNodeMutation  = loadModuleAtPath(path_DeleteNodeMutation, llvmContext);
+  auto module_ReplaceNodeMutation = loadModuleAtPath(path_ReplaceNodeMutation, llvmContext);
 
   ObjectLinkingLayer<> ObjLayer;
 
@@ -63,12 +80,28 @@ TEST(Sandbox, Test001) {
 
   SimpleCompiler compiler(*TM);
 
-  auto compiledModule_runnerTests = compiler(*module_runnerTests);
-  auto compiledModule_report = compiler(*module_report);
+  auto compiledModule_runnerTests              = compiler(*module_runnerTests);
+  auto compiledModule_ReplaceNodeMutationTests = compiler(*module_ReplaceNodeMutationTests);
+  auto compiledModule_DeleteNodeMutationTests  = compiler(*module_DeleteNodeMutationTests);
+
+  auto compiledModule_report              = compiler(*module_report);
+  auto compiledModule_NodeReplacement     = compiler(*module_NodeReplacement);
+  auto compiledModule_Runner              = compiler(*module_Runner);
+  auto compiledModule_MutationFactory     = compiler(*module_MutationFactory);
+  auto compiledModule_DeleteNodeMutation  = compiler(*module_DeleteNodeMutation);
+  auto compiledModule_ReplaceNodeMutation = compiler(*module_ReplaceNodeMutation);
 
   std::vector<object::ObjectFile*> objcSet;
   objcSet.push_back(compiledModule_runnerTests.getBinary());
+  objcSet.push_back(compiledModule_DeleteNodeMutationTests.getBinary());
+  objcSet.push_back(compiledModule_ReplaceNodeMutationTests.getBinary());
+
   objcSet.push_back(compiledModule_report.getBinary());
+  objcSet.push_back(compiledModule_NodeReplacement.getBinary());
+  objcSet.push_back(compiledModule_Runner.getBinary());
+  objcSet.push_back(compiledModule_MutationFactory.getBinary());
+  objcSet.push_back(compiledModule_DeleteNodeMutation.getBinary());
+  objcSet.push_back(compiledModule_ReplaceNodeMutation.getBinary());
 
   ObjCResolver objcResolver;
   auto objcHandle = ObjLayer.addObjectSet(std::move(objcSet),
